71d24f2d7d050ebdea5464210491b630072f6537
Yuanlong-Tony-Cui <yuanlongcui0511@gmail.com>
2022-11-10 17:50:27 -0500

    Finished everything except parsing the hex res on the client side.

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import random
 from socket import *
 
 
-def gen_dns_header(arg_msg_type):
+def gen_dns_header(arg_msg_type, arg_num_rr):
     hex_str = ""
 
     # 1. Generate ID:
@@ -13,7 +13,7 @@ def gen_dns_header(arg_msg_type):
         binary_str += str(random.randint(0, 1))
         i += 1
     # Binary -> integer -> "0x____"
-    hex_str += format(int(binary_str, 2), "#06x")
+    hex_str += format(int(binary_str, 2), "04x")
 
     # 2. Generate flags:
     binary_str = ""
@@ -25,14 +25,14 @@ def gen_dns_header(arg_msg_type):
     binary_str += "0000"  # 4-bit OPCODE
     binary_str += "1000"  # AA, TC, RD, and RA
     binary_str += "0000000"  # 3-bit ZZ and 4-bit RCODE
-    # Binary -> integer -> "0x____"
-    hex_str += format(int(binary_str, 2), "#06x")
+    # Binary -> integer -> "____"
+    hex_str += format(int(binary_str, 2), "04x")
 
     # 3. Generate counts:
-    hex_str += format(1, "#06x")  # QDCOUNT
-    hex_str += format(0, "#06x")  # ANCOUNT TODO [QUESTION]: 0 resource record for "query" messages?
-    hex_str += format(0, "#06x")  # NSCOUNT
-    hex_str += format(0, "#06x")  # ARCOUNT
+    hex_str += format(1, "04x")  # QDCOUNT
+    hex_str += format(arg_num_rr, "04x")  # ANCOUNT
+    hex_str += format(0, "04x")  # NSCOUNT
+    hex_str += format(0, "04x")  # ARCOUNT
 
     # print(hex_str)
     return hex_str
@@ -42,30 +42,31 @@ def gen_dns_question(arg_domain_name):
     hex_str = ""
 
     # 1. QNAME:
-    hex_str += format(len(arg_domain_name.split(".")[0]), "#04x")
+    hex_str += format(len(arg_domain_name.split(".")[0]), "02x")
     for letter in arg_domain_name:
         if letter == ".":
-            hex_str += format((len(arg_domain_name.split(".")[1])), "#04x")
+            hex_str += format((len(arg_domain_name.split(".")[1])), "02x")
         else:
-            # Convert char into integer then into "0x__":
-            hex_str += format(ord(letter), "#04x")
-    hex_str += format(0, "#04x")  # Terminated by "0x00"
+            # Convert char into integer then into "__":
+            hex_str += format(ord(letter), "02x")
+    hex_str += format(0, "02x")  # Terminated by "00"
 
     # 2. QTYPE & QCLASS:
-    hex_str += format(1, "#06x")
-    hex_str += format(1, "#06x")
+    hex_str += format(1, "04x")
+    hex_str += format(1, "04x")
 
     return hex_str
 
 
 def format_hex_str(arg_hex_str):
-    arg_hex_str = arg_hex_str.replace("0x", "")
     formatted_str = ""
     i = 0
     for hex_char in arg_hex_str:
         formatted_str += hex_char
         if i % 2 == 1 and i != len(arg_hex_str)-1:
             formatted_str += " "
+            if (i+1) % (16*2) == 0:
+                formatted_str += "\n"
         i += 1
     return formatted_str
 
@@ -78,29 +79,24 @@ while True:
     user_input = input("Enter Domain Name: ")
     if user_input == "end":
         clientSocket.close()
-        # TODO [QUESTION]: Any other actions needed to end the session?
         print("Session ended")
         break
 
-    # Create a DNS query message (in Hex)
-    dns_query = format_hex_str(gen_dns_header("query") + gen_dns_question(user_input))
+    # Create DNS query (in Hex):
+    dns_query = format_hex_str(gen_dns_header("query", 0) + gen_dns_question(user_input))
     print(dns_query)
+    clientSocket.sendto(dns_query.encode(), (serverIP, serverPort))
 
-    # clientSocket.sendto(user_input.encode(), (serverIP, serverPort))
+    # TODO: Parse the DNS response (hex string) into formatted client-side output:
+    server_res, serverAddress = clientSocket.recvfrom(2048)
+
     # server_res, serverAddress = clientSocket.recvfrom(2048)
-    # server_res = json.loads(server_res.decode())
     # # print(server_res)
     # output_str = ""
     # for ip_addr in server_res["IP address"]:
     #     output_str += user_input + ": type " + server_res["Type"] + ", class " + server_res["Class"] + \
     #                   ", TTL " + server_res["TTL"] + ", addr (4) " + ip_addr
     #     # TODO [QUESTION]: Do we need to print "Input from the user:" and "Output:"?
-    #     # TODO [QUESTION]: Isn't the IP address always 4-bytes?
     #     if (server_res["IP address"].index(ip_addr) + 1) != len(server_res["IP address"]):
     #         output_str += "\n"
     # print(output_str)
-    #
-    # # TODO: Parse the server response into a DNS response message (Hex)
-    # dns_res_header = dns_query_header
-    # dns_res_question = dns_query_question
-    # dns_res_answer = ""
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-import json
+import random
+# from client import gen_dns_header, gen_dns_question
 
 from socket import *
 serverIP = "127.0.0.1"
@@ -39,9 +40,125 @@ domain_name_dict["amazon.ca"].update({
 })
 # print("domain_name_dict:", domain_name_dict)
 
+
+def find_domain_name(arg_query_str):
+    query_hex_arr = arg_query_str.split(" ")
+    name = ""
+    for hex_item in query_hex_arr:
+        i = query_hex_arr.index(hex_item)
+        if i > 12:
+            if hex_item == "00":
+                break
+            if int(hex_item, 16) < 10:  # integer: 0 ~ 9
+                if i != 12:
+                    name += "."
+            else:   # chars
+                name += chr(int(hex_item, 16))
+    return name
+
+
+def gen_dns_header(arg_msg_type, arg_num_rr):
+    hex_str = ""
+
+    # 1. Generate ID:
+    binary_str = ""
+    i = 0
+    while i < 16:
+        binary_str += str(random.randint(0, 1))
+        i += 1
+    # Binary -> integer -> "0x____"
+    hex_str += format(int(binary_str, 2), "04x")
+
+    # 2. Generate flags:
+    binary_str = ""
+    # 1-bit QR:
+    if arg_msg_type == "query":
+        binary_str += "0"
+    elif arg_msg_type == "response":
+        binary_str += "1"
+    binary_str += "0000"  # 4-bit OPCODE
+    binary_str += "1000"  # AA, TC, RD, and RA
+    binary_str += "0000000"  # 3-bit ZZ and 4-bit RCODE
+    # Binary -> integer -> "____"
+    hex_str += format(int(binary_str, 2), "04x")
+
+    # 3. Generate counts:
+    hex_str += format(1, "04x")  # QDCOUNT
+    hex_str += format(arg_num_rr, "04x")  # ANCOUNT
+    hex_str += format(0, "04x")  # NSCOUNT
+    hex_str += format(0, "04x")  # ARCOUNT
+
+    # print(hex_str)
+    return hex_str
+
+
+def gen_dns_question(arg_domain_name):
+    hex_str = ""
+
+    # 1. QNAME:
+    hex_str += format(len(arg_domain_name.split(".")[0]), "02x")
+    for letter in arg_domain_name:
+        if letter == ".":
+            hex_str += format((len(arg_domain_name.split(".")[1])), "02x")
+        else:
+            # Convert char into integer then into "__":
+            hex_str += format(ord(letter), "02x")
+    hex_str += format(0, "02x")  # Terminated by "00"
+
+    # 2. QTYPE & QCLASS:
+    hex_str += format(1, "04x")
+    hex_str += format(1, "04x")
+
+    return hex_str
+
+
+def gen_dns_answer(arg_ttl, arg_ip_addr_arr):
+    hex_str = ""
+    for ip_addr in arg_ip_addr_arr:
+        hex_str += "c00c"   # NAME
+        hex_str += "0001"   # TYPE
+        hex_str += "0001"   # CLASS
+        hex_str += format(int(arg_ttl), "08x")    # TTL
+        # Count and parse the octets in each IP address:
+        hex_str += format(len(ip_addr.split(".")), "04x")
+        for num in ip_addr.split("."):
+            hex_str += format(int(num), "02x")
+    return hex_str
+
+
+def format_hex_str(arg_hex_str):
+    formatted_str = ""
+    i = 0
+    for hex_char in arg_hex_str:
+        formatted_str += hex_char
+        if i % 2 == 1 and i != len(arg_hex_str)-1:
+            formatted_str += " "
+            if (i+1) % (16*2) == 0:
+                formatted_str += "\n"
+        i += 1
+    return formatted_str
+
+
 while True:
     client_req, clientAddress = serverSocket.recvfrom(2048)
-    print(client_req, clientAddress)
-    domain_name = client_req.decode()
-    serverSocket.sendto(json.dumps(domain_name_dict[domain_name]).encode(), clientAddress)
+    # print(client_req, clientAddress)
+    query_hex_str = client_req.decode()
+    domain_name = find_domain_name(query_hex_str)
+    # print(domain_name)
+    print("Request:")
+    print(query_hex_str)
+
+    dns_res_header = gen_dns_header(
+        "response",
+        len(domain_name_dict[domain_name]["IP address"])
+    )
+    dns_res_question = gen_dns_question(domain_name)
+    dns_res_answer = gen_dns_answer(
+        domain_name_dict[domain_name]["TTL"],
+        domain_name_dict[domain_name]["IP address"]
+    )
+    dns_response = format_hex_str(dns_res_header + dns_res_question + dns_res_answer)
+    print("Response:")
+    print(dns_response)
+    serverSocket.sendto(dns_response.encode(), clientAddress)
 
